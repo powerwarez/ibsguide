205be89972a67589a573a38ddbd9a606f5a3ab50
powerwarez <powerware@naver.com>
2024-11-14 16:01:59 +0900

    쿼터손절모드트랜잭션카운터 초기값 -1로 수정

--- src/components/AddStock.jsx
+++ src/components/AddStock.jsx
@@ -11,7 +11,7 @@ const AddStock = ({ onAdd }) => {
   const [profit, setProfit] = useState(0); // 현재 수익
   const [isSettled] = useState(false); // 정산 여부 (초기값 false)
   const [quarterCutMode] = useState(false);
-  const [cutModetransactionCounter] = useState(0);
+  const [cutModetransactionCounter] = useState(-1);
 
   // 투자 금액과 분할 횟수에 따라 1회 매수금을 계산
   useEffect(() => {
