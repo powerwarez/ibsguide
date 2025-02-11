f26a851ea264201e219616320d427fd83689a5c8
powerwarez <powerware@naver.com>
2025-02-11 15:32:25 +0900

    트랜잭션테이블 표시 내용 수정(인덱스 삭제)

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -5,9 +5,8 @@ const TransactionTable = ({ transactions, isSettled, onDeleteClick }) => {
     <table className="w-full border-collapse mt-6">
       <thead>
         <tr className="bg-gray-200">
-          <th className="p-2 border">No</th>
-          <th className="p-2 border">유형</th>
           <th className="p-2 border">날짜</th>
+          <th className="p-2 border">유형</th>
           <th className="p-2 border">체결가</th>
           <th className="p-2 border">수량</th>
           <th className="p-2 border">수수료</th>
@@ -17,20 +16,19 @@ const TransactionTable = ({ transactions, isSettled, onDeleteClick }) => {
       <tbody>
         {transactions.map((transaction, index) => (
           <tr key={transaction.id || index} className="text-center">
-            <td className="p-2 border">{index + 1}</td>
-            <td
-              className={`p-2 border ${
-                transaction.type === "매수" ? "text-red-500" : "text-blue-500"
-              }`}
-            >
-              {transaction.type}
-            </td>
             {/* <td className="p-2 border">{transaction.date.replace(/(\d{4})-(\d{2})-(\d{2})/, (match, p1, p2, p3) => `${p1.slice(2)}.${p2}.${p3}.`)}</td> */}
             <td className="p-2 border">
               {transaction.date.replace(
                 /(\d{4})-(\d{2})-(\d{2})/,
                 (match, p1, p2, p3) => `${p2}.${p3}.`
               )}
+            </td>
+            <td
+              className={`p-2 border ${
+                transaction.type === "매수" ? "text-red-500" : "text-blue-500"
+              }`}
+            >
+              {transaction.type}
             </td>
             <td className="p-2 border">${transaction.price}</td>
             <td className="p-2 border">{Math.abs(transaction.quantity)}</td>
