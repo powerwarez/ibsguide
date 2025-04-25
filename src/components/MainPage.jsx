e069ad1dfadcba55e60965dc6becd53ca0a103b3
powerwarez <powerware@naver.com>
2025-04-25 14:17:59 +0900

    하단 여백 추가 mb-6

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -84,7 +84,7 @@ const MainPage = () => {
           });
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-md">
+    <div className="max-w-4xl mx-auto p-6 mb-6 bg-white shadow-md rounded-md">
       <h1 className="text-3xl font-bold mb-6 text-center">
         무한매수 도우미{" "}
         <span className="text-gray-500 text-sm ml-2">updated 25.04.19.</span>
